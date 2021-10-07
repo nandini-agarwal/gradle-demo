57f6d9ac031c048330acd1ea8698f1fa0b865271
Nandini Agarwal <nandini.agarwal98@gmail.com>
2021-10-07 20:32:19 +0530

    Set-up commit

---
+++ src/test/java/com/nandoz/bits/devops/gradledemo/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandoz.bits.devops.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
